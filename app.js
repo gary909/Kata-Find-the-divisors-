e3f1fc87abab90a5486619ca186f34179b991380
gary909 <garywhite61@hotmail.com>
2022-09-21 13:37:41 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 function divisors(integer) {
+    let myArr = [];
 
+    // divisors check
+    for (let i = 2; i < integer; i++) {
+        if (integer % i == 0) {
+            myArr.push(i);
+        }
+    }
+    // prime check
+    if (myArr.length == 0) {
+        return integer + " is prime"
+    } else {
+        return myArr;
+    }
 };
 
 console.log(divisors(15)); // [3, 5]
